cdcad1e129036bab338a54d099085f196b043b06
Jonathan Anderson <jranderson404@gmail.com>
2025-05-09 17:32:07 -0400

    fixing verify_secret

--- profile_db/app.py
+++ profile_db/app.py
@@ -264,4 +264,4 @@ if __name__ == '__main__':
     # Create database tables if they don't exist
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5003, ssl_context=('/certs/fullchain.pem', '/certs/privkey.pem'))
+    app.run(host='0.0.0.0', port=5003)
