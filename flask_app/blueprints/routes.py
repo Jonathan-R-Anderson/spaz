0006e1c2ecdb3792b0cc30eb36b1e1456cccf8ab
Jonathan Anderson <jranderson404@gmail.com>
2025-05-10 14:41:17 -0400

    refactoring

--- flask_app/blueprints/routes.py
+++ flask_app/blueprints/routes.py
@@ -162,7 +162,7 @@ def generate_rtmp_url():
 def get_magnet_url(eth_address):
     logging.info(f"Fetching magnet URLs for {eth_address}")
     try:
-        response = requests.get(f"{WEBTORRENT_CONTAINER_URL}/magnet_urls/{eth_address}", timeout=30)
+        response = requests.get(f"{WEBTORRENT_CONTAINER_URL}/magnet_urls/{eth_address}", timeout=30, verify=False)
         if response.status_code == 200:
             return jsonify({"magnet_urls": response.json().get("magnet_urls")}), 200
         return jsonify({"error": "Failed to generate magnet URLs"}), 500
