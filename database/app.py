d9d0bf2247e7c6dce08744b576950e0b69d4dbee
Jonathan Anderson <jranderson404@gmail.com>
2025-05-12 20:24:28 -0400

    fixing app

--- database/app.py
+++ database/app.py
@@ -2,17 +2,48 @@
 from flask import Flask
 from config import Config
 from extensions import db, redis_client
-from models import User, MagnetURL
-from blueprints.routes import blueprint
+import logging
+from logging.config import dictConfig
 
-app = Flask(__name__)
-app.config.from_object(Config)
+# Centralized logging setup
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.FileHandler',
+            'filename': Config.LOG_FILE_PATH,
+            'formatter': 'default',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi', 'console']
+    }
+})
 
-db.init_app(app)
 
-app.register_blueprint(blueprint)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    # Avoid circular imports by importing here
+    from api.routes import blueprint
+    app.register_blueprint(blueprint)
+
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=5003, debug=True)
