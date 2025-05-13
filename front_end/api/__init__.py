892baf0a048e5bda3bb7a409cf7175240534901c
Jonathan Anderson <jranderson404@gmail.com>
2025-05-13 18:01:32 -0400

    repairing imports

--- front_end/api/__init__.py
+++ front_end/api/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_restful import Api
 from .routes import blueprint
 import os
+from config import Config
 
 api = Api()
 
 def create_app():
     app = Flask(__name__, static_url_path='')
-    app.config.from_object('config.Config')
+    app.config.from_object(Config)
     app.config['TEMPLATES_AUTO_RELOAD'] = os.getenv('TEMPLATES_AUTO_RELOAD', True)
     app.register_blueprint(blueprint)
     app.url_map.strict_slashes = False
