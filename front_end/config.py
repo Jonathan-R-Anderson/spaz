0fa9f8b96be5c1f9ecfae077c5fef77207bcee56
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 16:40:33 -0400

    debugging

--- front_end/config.py
+++ front_end/config.py
@@ -4,7 +4,7 @@ class Config:
     LOG_FILE_PATH = os.path.join("logs", "app.log")
 
     WEBTORRENT_CONTAINER_URL = f"{os.getenv('WEBTORRENT_CONTAINER_URL', 'https://webtorrent')}:{os.getenv('WEBTORRENT_PORT', 5002)}"
-    DATABASE_URL = f"{os.getenv('DATABASE_URL', 'http://database')}:{os.getenv('DATABASE_PORT', 5003)}"
+    DATABASE_URL = f"{os.getenv('DATABASE_URI', 'database')}:{os.getenv('DATABASE_PORT', 5003)}"
     HMAC_SECRET_KEY = os.getenv('HMAC_SECRET_KEY', '11257560')
     FILE_DIR = os.getenv('FILE_DIR', 'hosted')
     TRACKER_PORT = os.getenv('TRACKER_PORT', 5000)
