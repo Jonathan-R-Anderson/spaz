6edef7cfc1f5fae85014c10ec2d9e128944bb510
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 18:27:46 -0400

    broken static file references

--- front_end/api/__init__.py
+++ front_end/api/__init__.py
@@ -1,16 +1,26 @@
-from flask import Flask
-from flask_restful import Api
-from .routes import blueprint
 import os
-from config import Config
+from flask import Flask
+from config import Config 
+from api.routes import blueprint
+from flask_restful import Api
 
 api = Api()
 
 def create_app():
-    app = Flask(__name__, static_url_path='')
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(base_dir, 'templates'),
+        static_folder=os.path.join(base_dir, 'static'),
+        static_url_path='/static'
+    )
+
     app.config.from_object(Config)
     app.config['TEMPLATES_AUTO_RELOAD'] = os.getenv('TEMPLATES_AUTO_RELOAD', True)
+
     app.register_blueprint(blueprint)
     app.url_map.strict_slashes = False
     api.init_app(app)
+
     return app
