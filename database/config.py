b2beee7c3a37e7855d025a4eaf58c06019508e81
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 13:25:32 -0400

    fixing logggin

--- database/config.py
+++ database/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev")
-    LOG_FILE_PATH = os.getenv("LOG_FILE_PATH", "logs/app.log")
+    LOG_FILE_PATH = os.getenv("DATABASE_LOG_PATH", "logs/database.log")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://admin:admin@localhost/rtmp_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     HMAC_SECRET_KEY = os.getenv('HMAC_SECRET_KEY', '11257560')
