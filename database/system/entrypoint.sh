0314d9555621c3d8c905b273f936097e1f8dd5bc
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 22:41:01 -0400

    debugging

--- database/system/entrypoint.sh
+++ database/system/entrypoint.sh
@@ -16,11 +16,5 @@ chown postgres:postgres "$POSTGRES_CONF"
 # Start Redis
 service redis-server start
 
-# Wait for PG to be ready
-until pg_isready -h localhost -U "$POSTGRES_USER"; do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
-
 # Create tables
 python3 /app/driver.py --init-db
