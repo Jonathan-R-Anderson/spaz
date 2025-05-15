ba413714a558ed2f752a820a3caa4360fc02afdf
Jonathan Anderson <jranderson404@gmail.com>
2025-05-15 18:32:04 -0400

    forgot about variable rename

--- rtmp/config.py
+++ rtmp/config.py
@@ -11,6 +11,6 @@ class Config:
     HLS_FOLDER = os.path.join(STATIC_FOLDER, 'hls')
 
     # External service URLs (can be overridden via .env)
-    WEBTORRENT_CONTAINER_URL = f"{os.getenv('WEBTORRENT_CONTAINER_URL', 'https://webtorrent')}:{os.getenv('WEBTORRENT_PORT', 5002)}"
+    WEBTORRENT_CONTAINER_URL = f"{os.getenv('WEBTORRENT_URI', 'http://webtorrent')}:{os.getenv('WEBTORRENT_PORT', 5002)}"
     DATABASE_URL = f"{os.getenv('DATABASE_URL', 'http://database')}:{os.getenv('DATABASE_PORT', 5003)}"
     HOSTNAME = os.getenv("HOSTNAME", "http://psichos.is")
