bd3e7f563550fe7123ca6677f8856607356c95ef
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 22:21:15 -0400

    massive change

--- database/system/entrypoint.sh
+++ database/system/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -ex
 
+chown -R postgres:postgres /var/lib/postgresql
+
+# If config is missing, copy default
+if [ ! -f /var/lib/postgresql/data/postgresql.conf ]; then
+  cp /tmp/postgresql.conf /var/lib/postgresql/data/postgresql.conf
+  chown postgres:postgres /var/lib/postgresql/data/postgresql.conf
+fi
+
 # Start Redis
 service redis-server start
 
