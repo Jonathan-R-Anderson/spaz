727d203beb2015508c6a53b91b320ce6b4a99720
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 20:41:47 -0400

    db import

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -2,3 +2,7 @@ from .user import Users
 from .magnet import MagnetURL
 
 __all__ = ['Users', 'MagnetURL']
+
+# Also ensure the modules are loaded for SQLAlchemy to register them
+from . import user
+from . import magnet
