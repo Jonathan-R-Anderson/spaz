968ff5a5580702f29376df62f3678795adf62b3c
Jonathan Anderson <jranderson404@gmail.com>
2025-05-14 20:45:14 -0400

    db import

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,8 +1,11 @@
+# models/__init__.py
+
+# These two lines force SQLAlchemy to evaluate the model classes
+from . import user
+from . import magnet
+
+# These make them importable elsewhere
 from .user import Users
 from .magnet import MagnetURL
 
 __all__ = ['Users', 'MagnetURL']
-
-# Also ensure the modules are loaded for SQLAlchemy to register them
-from . import user
-from . import magnet
