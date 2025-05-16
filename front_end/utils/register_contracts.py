537cb64b435b9fce55b8b0d76b05424351786701
Jonathan Anderson <jranderson404@gmail.com>
2025-05-16 16:13:50 -0400

    import revision

--- front_end/utils/register_contracts.py
+++ front_end/utils/register_contracts.py
@@ -1,10 +1,11 @@
-# register_contracts.py
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import time
 import json
-import os
 import requests
 from config import Config
-from ..system.logging import setup_logger
+from system.logging import setup_logger
 
 logger = setup_logger("spaz_register")
 
