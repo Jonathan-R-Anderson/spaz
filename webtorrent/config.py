79c9c9ff956f7016e6f8b30eaf5f1e37ff399e51
Jonathan Anderson <jranderson404@gmail.com>
2025-05-15 23:39:01 -0400

    fixed broken reference

--- webtorrent/config.py
+++ webtorrent/config.py
@@ -7,7 +7,7 @@ class Config:
     SEED_THRESHOLD = int(os.getenv("SEED_THRESHOLD", 10))
     STATIC_FOLDER = '/app/static'
     HLS_FOLDER = os.path.join(STATIC_FOLDER, 'hls')
-    DATABASE_URL = f"{os.getenv('DATABASE_URL', 'http://database')}:{os.getenv('DATABASE_PORT', 5003)}"
+    DATABASE_URI = f"{os.getenv('DATABASE_URI', 'http://database')}:{os.getenv('DATABASE_PORT', 5003)}"
 
     magnet_url_lock = Lock()
     is_monitoring_static = {}
