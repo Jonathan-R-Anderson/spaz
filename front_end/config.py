eacea6de4db511c9b4efc9e24c48c102cebd9431
Jonathan Anderson <jranderson404@gmail.com>
2025-05-16 05:16:07 -0400

    removed https

--- front_end/config.py
+++ front_end/config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     LOG_FILE_PATH = os.path.join("logs", "app.log")
 
-    WEBTORRENT_URI = f"{os.getenv('WEBTORRENT_URI', 'https://webtorrent')}:{os.getenv('WEBTORRENT_PORT', 5002)}"
+    WEBTORRENT_URI = f"{os.getenv('WEBTORRENT_URI', 'http://webtorrent')}:{os.getenv('WEBTORRENT_PORT', 5002)}"
     DATABASE_URI = f"{os.getenv('DATABASE_URI', 'database')}:{os.getenv('DATABASE_PORT', 5003)}"
     HMAC_SECRET_KEY = os.getenv('HMAC_SECRET_KEY', '11257560')
     TRACKER_PORT = os.getenv('TRACKER_PORT', 5000)
-    BLOCKCHAIN_URI = f"{os.getenv('BLOCKCHAIN_URI', 'https://blockchain')}:{os.getenv('BLOCKCHAIN_PORT', 5005)}"
+    BLOCKCHAIN_URI = f"{os.getenv('BLOCKCHAIN_URI', 'http://blockchain')}:{os.getenv('BLOCKCHAIN_PORT', 5005)}"
     SECURE_MESSAGE = "Please sign this message to verify ownership of your Ethereum address."
     SHARED_DIR = os.getenv("SHARED_DIR", "/app")
     FILE_DIR = os.path.join(SHARED_DIR, "uploads")
