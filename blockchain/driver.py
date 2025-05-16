d7a9a6e879eb1082511c96c31326531677a7ad34
Jonathan Anderson <jranderson404@gmail.com>
2025-05-16 16:06:44 -0400

    blueprint

--- blockchain/driver.py
+++ blockchain/driver.py
@@ -4,7 +4,9 @@ import json
 import requests
 from flask import Flask
 from dotenv import load_dotenv
-from api.routes import blueprint
+
+# Use the specific route you want
+from api.views.register_contracts import register_contracts_routes
 from system.logging import setup_logger
 from config import Config
 
@@ -23,9 +25,8 @@ os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
 logger.info("[BOOT] Logger initialized")
 logger.info(f"[BOOT] HELP_PORT set to {HELP_PORT}")
 
-
 if __name__ == "__main__":
     app = Flask(__name__)
-    app.register_blueprint(blueprint)
-    logger.info("[FLASK] Blueprint registered")
-    app.run(host="0.0.0.0", port=5005)
+    app.register_blueprint(register_contracts_routes)
+    logger.info("[FLASK] register_contracts_routes blueprint registered")
+    app.run(host="0.0.0.0", port=HELP_PORT)
