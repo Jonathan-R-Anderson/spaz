44183d731c962d3dae5e1c479512b8f8262f0a07
Jonathan Anderson <jranderson404@gmail.com>
2025-05-16 13:54:48 -0400

    refactoring

--- front_end/config.py
+++ front_end/config.py
@@ -6,7 +6,7 @@ class Config:
     LOG_FILE_PATH = os.path.join("logs", "app.log")
 
     WEBTORRENT_URI = f"{os.getenv('WEBTORRENT_URI', 'http://webtorrent')}:{os.getenv('WEBTORRENT_PORT', 5002)}"
-    BLOCKCHAIN_URI = f"{os.getenv('BLOCKCHAIN_URI', 'http://blockchain')}:{os.getenv('BLOCKCHAIN_PORT', '5005')}"
+    BLOCKCHAIN_URI = f"{os.getenv('BLOCKCHAIN_URI', 'http://blockchain')}:{os.getenv('BLOCKCHAIN_PORT', 5005)}"
     DATABASE_URI = f"{os.getenv('DATABASE_URI', 'database')}:{os.getenv('DATABASE_PORT', 5003)}"
     HMAC_SECRET_KEY = os.getenv('HMAC_SECRET_KEY', '11257560')
     TRACKER_PORT = os.getenv('TRACKER_PORT', 5000)
