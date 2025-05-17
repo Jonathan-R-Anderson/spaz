efd6eb797a4c4c9dcb73c7b7a71c51da365dcefa
Jonathan Anderson <jranderson404@gmail.com>
2025-05-17 15:00:19 -0400

    cleaning up imports

--- front_end/api/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint('main', __name__)
-
-# Register views from individual modules
-from .views import dashboard, rtmp, magnet, session, internal
--- front_end/api/routes/__init__.py
+++ front_end/api/routes/__init__.py
@@ -1,4 +1,13 @@
+from flask import Blueprint
+
+# Define the main blueprint for views
+blueprint = Blueprint('main', __name__)
+
+# Import view modules to register their routes
+from api.routes.views import dashboard, rtmp, magnet, session, internal
+
+# Import sub-blueprints
 from .verify_proxy import verify_bp
 from .dynamic_loader import dynamic_bp
 
-__all__ = ['verify_bp', 'dynamic_bp']
+__all__ = ['blueprint', 'verify_bp', 'dynamic_bp']
