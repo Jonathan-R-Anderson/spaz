be8fbab7925f5067564df2a200c0d3e632bab013
Jonathan Anderson <jranderson404@gmail.com>
2025-05-17 18:05:31 -0400

    refactoring

--- front_end/static/loading/src/main.tsx
+++ front_end/static/loading/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/static/apps/welcome/">
+    <BrowserRouter basename="/static/apps/loading/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
