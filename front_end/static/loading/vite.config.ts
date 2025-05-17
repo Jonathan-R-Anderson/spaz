f0c4ccc9f64317baace6476675c5698483dca5a5
Jonathan Anderson <jranderson404@gmail.com>
2025-05-17 19:09:12 -0400

    refactoring

--- front_end/static/loading/vite.config.ts
+++ front_end/static/loading/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/static/apps/loading/",
 
   server: {
     host: "::",
