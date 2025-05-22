3ecace4e5ce2fef90c492e114b0a84e3fe2bbd2d
Jonathan Anderson <jranderson404@gmail.com>
2025-05-21 22:37:13 -0400

    kerberos

--- vpn/config_server.py
+++ vpn/config_server.py
@@ -1,18 +1,9 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template, send_file
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-app = Flask(__name__)
-
-TEMPLATE = '''
-<h2>OpenVPN Initial Setup</h2>
-<form method="POST">
-  Client Name: <input name="client"><br><br>
-  <input type="submit" value="Generate OVPN">
-</form>
-'''
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET", "POST"])
 def setup():
@@ -21,14 +12,21 @@ def setup():
         os.system(f"easyrsa build-client-full {client} nopass")
         os.system(f"ovpn_getclient {client} > /etc/openvpn/{client}.ovpn")
         return f"<h3>Config created for {client}</h3><a href='/{client}.ovpn'>Download</a>"
-    return render_template_string(TEMPLATE)
+    return render_template("index.html")
 
 @app.route("/<client>.ovpn")
 def download(client):
-    return open(f"/etc/openvpn/{client}.ovpn").read(), 200, {
-        "Content-Type": "application/x-openvpn-profile",
-        "Content-Disposition": f"attachment; filename={client}.ovpn"
-    }
+    return send_file(
+        f"/etc/openvpn/{client}.ovpn",
+        mimetype="application/x-openvpn-profile",
+        as_attachment=True,
+        download_name=f"{client}.ovpn"
+    )
+
+@app.route("/shutdown", methods=["POST"])
+def shutdown():
+    os.system("kill 1") 
+    return "Shutting down...", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
