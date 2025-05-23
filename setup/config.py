039e6839486b3f6a1388479f33607288d7327fa3
Jonathan Anderson <jranderson404@gmail.com>
2025-05-23 13:40:39 -0400

    setup refactor

--- setup/config.py
+++ setup/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Config:
     REALM = os.getenv("REALM", "EXAMPLE.COM").upper()
     DOMAIN = os.getenv("DOMAIN", "vpn.localhost").lower()
-    PRINCIPAL = os.getenv("PRINCIPAL", f"HTTP/{self.DOMAIN}@{self.REALM}")
+    PRINCIPAL = os.getenv("PRINCIPAL", f"HTTP/{DOMAIN}@{REALM}")
     KEYTAB_PATH = os.getenv("KEYTAB_PATH", "/kerberos/output/service.keytab")
     MASTER_PASS = os.getenv("KRB5_MASTER_PASSWORD")
